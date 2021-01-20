b3bd97ab56bee3fe5812c2e39fee2b81932563ab
Burak BALDIRLIOGLU <burak.baldirlioglu@craftgate.io>
2021-01-20 14:21:15 +0300

    removes toUpperCase from signature calculation (#7)

--- lib/Craftgate/Util/AuthSignatureGenerator.php
+++ lib/Craftgate/Util/AuthSignatureGenerator.php
@@ -12,6 +12,6 @@ class AuthSignatureGenerator
         }
 
         $hashStr = $requestOptions->getBaseUrl() . urldecode($path) . $requestOptions->getApiKey() . $requestOptions->getSecretKey() . $randomString . $requestString;
-        return strtoupper(base64_encode(hash("sha256", $hashStr, true)));
+        return base64_encode(hash("sha256", $hashStr, true));
     }
 }
--- unit-tests/Craftgate/Tests/Util/AuthSignatureGeneratorTest.php
+++ unit-tests/Craftgate/Tests/Util/AuthSignatureGeneratorTest.php
@@ -14,7 +14,7 @@ class AuthSignatureGeneratorTest extends \PHPUnit_Framework_TestCase
         $options->setSecretKey('secret-key');
         $options->setBaseUrl('http://localhost:8000');
 
-        $this->assertEquals("UK7LKWOEZVH+PX/YOIMUSXVPKHLR4KOA7PUYKXCYOVQ=", AuthSignatureGenerator::generateSignature($options, "/installment/v1/installments", "1234", null));
+        $this->assertEquals("uK7LkWOEzVH+Px/YOiMuSXvPkHLR4KoA7PuykXcYovQ=", AuthSignatureGenerator::generateSignature($options, "/installment/v1/installments", "1234", null));
     }
 
     public function test_should_generate_signature_with_body()
@@ -28,7 +28,7 @@ class AuthSignatureGeneratorTest extends \PHPUnit_Framework_TestCase
             'cardUserKey' => 'de050909-39a9-473c-a81a-f186dd55cfef'
         );
 
-        $this->assertEquals("KQM62AFJXCW6RS6QBCLXTA9TV1GQD/SXSALYP+CEHG8=", AuthSignatureGenerator::generateSignature($options, "/payment/v1/cards", "1234", $request));
+        $this->assertEquals("kQm62AfjXCw6rS6QBCLXta9tV1GqD/SXsAlYP+cEhG8=", AuthSignatureGenerator::generateSignature($options, "/payment/v1/cards", "1234", $request));
     }
 
     public function test_should_generate_signature_with_encoded_query_string()
@@ -38,6 +38,6 @@ class AuthSignatureGeneratorTest extends \PHPUnit_Framework_TestCase
         $options->setSecretKey('secret-key');
         $options->setBaseUrl('http://localhost:8000');
 
-        $this->assertEquals("VXYA+5LH3F4M8THQA2LPVBWZVGCQGRHUE4XAGCVJJYQ=", AuthSignatureGenerator::generateSignature($options, "/onboarding/v1/members?name=Zeytinya%C4%9F%C4%B1%20%C3%9Cretim", "1234", null));
+        $this->assertEquals("vxYA+5LH3F4m8tHQA2LpVBwzVgCqGRHue4XAgcVjjYQ=", AuthSignatureGenerator::generateSignature($options, "/onboarding/v1/members?name=Zeytinya%C4%9F%C4%B1%20%C3%9Cretim", "1234", null));
     }
 }
