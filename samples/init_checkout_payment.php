add2c260ab7339c5fd899fca2c43d44859372fbe
Berkay Dinçer <berkay.dincer@craftbase.io>
2021-01-22 14:22:18 +0300

    adds common payment page methods

---
+++ samples/init_checkout_payment.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('config/sample_config.php');
+
+use Craftgate\Model\Currency;
+use Craftgate\Model\PaymentGroup;
+
+$request = array(
+    'price' => 100,
+    'paidPrice' => 100,
+    'walletPrice' => 0,
+    'installment' => 1,
+    'currency' => Currency::TL,
+    'paymentGroup' => PaymentGroup::LISTING_OR_SUBSCRIPTION,
+    'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5',
+    'items' => array(
+        array(
+            'externalId' => uniqid(),
+            'name' => 'Item 1',
+            'price' => 30
+        ),
+        array(
+            'externalId' => uniqid(),
+            'name' => 'Item 2',
+            'price' => 50
+        ),
+        array(
+            'externalId' => uniqid(),
+            'name' => 'Item 3',
+            'price' => 20
+        )
+    )
+);
+
+$response = FunctionalTestConfig::craftgate()->payment()->initCheckoutPayment($request);
+
+print_r($response);
