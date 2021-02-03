857544da68b47b60ac3d0c7517ea5902517e980c
Kerem Güneş <qeremy@gmail.com>
2021-02-03 21:34:09 +0300

    Make-ups on constant names.

--- autoload.php
+++ autoload.php
@@ -3,8 +3,8 @@
 namespace Craftgate;
 
 spl_autoload_register(function ($name) {
-    if (strpos($name, __namespace__) === 0) {
-        $name = substr($name, strlen(__namespace__) + 1);
+    if (strpos($name, __NAMESPACE__) === 0) {
+        $name = substr($name, strlen(__NAMESPACE__) + 1);
         $name = strtr($name, '\\', DIRECTORY_SEPARATOR);
         ($file = realpath(
             __DIR__ . DIRECTORY_SEPARATOR
