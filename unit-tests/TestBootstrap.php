9807c1b593416c6f0045794830a1ee4f027366bf
Sabri Onur Tüzün <onur@craftgate.io>
2021-02-09 11:56:08 +0300

    updates test bootstrap

--- unit-tests/TestBootstrap.php
+++ unit-tests/TestBootstrap.php
@@ -3,8 +3,6 @@
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 $loader->addPsr4('Craftgate\\Tests\\', __DIR__ . '/');
 
-date_default_timezone_set('UTC');
-
 if (!function_exists('each')) {
     function each(array &$array)
     {
@@ -21,3 +19,19 @@ if (!function_exists('each')) {
         return array(1 => $value, 'value' => $value, 0 => $key, 'key' => $key);
     }
 }
+
+/*
+// Yields same results with the samples on docs: https://php.net/each
+$foo = array("bob", "fred", "jussi", "jouni", "egon", "marliese");
+print_r(each($foo));
+$foo = array("Robert" => "Bob", "Seppo" => "Sepi");
+print_r(each($foo));
+print_r(each($foo));
+print_r(each($foo)); // false
+$fruit = array('a' => 'apple', 'b' => 'banana', 'c' => 'cranberry');
+while (list($key, $val) = each($fruit)) {
+    echo "$key => $val\n";
+}
+*/
+
+date_default_timezone_set('UTC');
