1819f4dc78037d926cc0dcf7a064002fcd144075
Sabri Onur Tüzün <onur@craftgate.io>
2021-02-10 20:25:30 +0300

    sets timeout settings

--- src/Util/Curl.php
+++ src/Util/Curl.php
@@ -4,10 +4,15 @@ namespace Craftgate\Util;
 
 class Curl
 {
+    const CONNECT_TIMEOUT = 10;
+    const READ_TIMEOUT = 150;
+
     public static function get($url, array $headers)
     {
         return self::request($url, array(
             CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_CONNECTTIMEOUT => self::CONNECT_TIMEOUT,
+            CURLOPT_TIMEOUT => self::READ_TIMEOUT,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => $headers,
         ));
@@ -17,6 +22,8 @@ class Curl
     {
         return self::request($url, array(
             CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_CONNECTTIMEOUT => self::CONNECT_TIMEOUT,
+            CURLOPT_TIMEOUT => self::READ_TIMEOUT,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_POSTFIELDS => json_encode($content),
@@ -27,6 +34,8 @@ class Curl
     {
         return self::request($url, array(
             CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_CONNECTTIMEOUT => self::CONNECT_TIMEOUT,
+            CURLOPT_TIMEOUT => self::READ_TIMEOUT,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => $headers,
             CURLOPT_POSTFIELDS => json_encode($content),
@@ -37,6 +46,8 @@ class Curl
     {
         return self::request($url, array(
             CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_CONNECTTIMEOUT => self::CONNECT_TIMEOUT,
+            CURLOPT_TIMEOUT => self::READ_TIMEOUT,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HTTPHEADER => $headers,
         ));
@@ -45,6 +56,7 @@ class Curl
     private static function request($url, $options)
     {
         $request = curl_init($url);
+
         curl_setopt_array($request, $options);
 
         $response = curl_exec($request);
