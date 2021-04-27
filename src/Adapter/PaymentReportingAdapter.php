6731992fc51dcd684a51a4cfbe95b18d161294b0
Sabri Onur Tüzün <335067+sotuzun@users.noreply.github.com>
2021-04-27 17:40:33 +0300

    implements payment reporting api adapter methods (#25)
    * implements payment reporting api adapter methods
    
    * renames methods
    
    * updates sample data
    
    * removes credentials

--- src/Adapter/PaymentReportingAdapter.php
+++ src/Adapter/PaymentReportingAdapter.php
@@ -6,9 +6,45 @@ use Craftgate\Util\QueryBuilder;
 
 class PaymentReportingAdapter extends BaseAdapter
 {
-    public function searchPayments($request)
+    public function searchPayments(array $request)
     {
         $path = "/payment-reporting/v1/payments" . QueryBuilder::build($request);
+        return $this->httpGet($path);
+    }
+
+    public function retrievePayment($paymentId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId;
+        return $this->httpGet($path);
+    }
+
+    public function retrievePaymentTransactions($paymentId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId . "/transactions";
+        return $this->httpGet($path);
+    }
+
+    public function retrievePaymentRefunds($paymentId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId . "/refunds";
+        return $this->httpGet($path);
+    }
+
+    public function retrievePaymentTransactionRefunds($paymentId, $paymentTransactionId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId . "/transactions/" . $paymentTransactionId . "/refunds";
+        return $this->httpGet($path);
+    }
+
+    public function searchPaymentRefunds(array $request)
+    {
+        $path = "/payment-reporting/v1/refunds" . QueryBuilder::build($request);
+        return $this->httpGet($path);
+    }
+
+    public function searchPaymentTransactionRefunds(array $request)
+    {
+        $path = "/payment-reporting/v1/refund-transactions" . QueryBuilder::build($request);
         return $this->httpGet($path);
     }
 }
