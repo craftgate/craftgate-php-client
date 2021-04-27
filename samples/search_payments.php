2a212d2fd1fec2641e135d8351caf9f8646cad51
Sabri Onur Tüzün <onur@craftgate.io>
2021-04-27 10:30:07 +0300

    implements payment reporting api adapter methods

--- samples/search_payments.php
+++ samples/search_payments.php
@@ -2,7 +2,6 @@
 
 require_once('config/sample_config.php');
 
-
 $request = array(
     'startDate' => date_create()->modify('-4 days')->format('Y-m-d\TH:i:s'),
     'endDate' => date_create()->format('Y-m-d\TH:i:s')
--- src/Adapter/PaymentReportingAdapter.php
+++ src/Adapter/PaymentReportingAdapter.php
@@ -11,4 +11,40 @@ class PaymentReportingAdapter extends BaseAdapter
         $path = "/payment-reporting/v1/payments" . QueryBuilder::build($request);
         return $this->httpGet($path);
     }
+
+    public function retrievePayment($paymentId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId;
+        return $this->httpGet($path);
+    }
+
+    public function listPaymentTransactions($paymentId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId . "/transactions";
+        return $this->httpGet($path);
+    }
+
+    public function listPaymentRefunds($paymentId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId . "/refunds";
+        return $this->httpGet($path);
+    }
+
+    public function listPaymentTransactionRefunds($paymentId, $paymentTransactionId)
+    {
+        $path = "/payment-reporting/v1/payments/" . $paymentId . "/transactions/" . $paymentTransactionId . "/refunds";
+        return $this->httpGet($path);
+    }
+
+    public function searchRefunds($request)
+    {
+        $path = "/payment-reporting/v1/refunds" . QueryBuilder::build($request);
+        return $this->httpGet($path);
+    }
+
+    public function searchPaymentTransactionRefunds($request)
+    {
+        $path = "/payment-reporting/v1/refund-transactions" . QueryBuilder::build($request);
+        return $this->httpGet($path);
+    }
 }
