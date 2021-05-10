a7deba6315578f57404973a464a9ffa1859caf2a
Alican Akkus <alican.akkus@craftbase.io>
2021-05-10 15:54:10 +0300

    adds retrieve payout details method

--- src/Adapter/SettlementReportingAdapter.php
+++ src/Adapter/SettlementReportingAdapter.php
@@ -17,4 +17,10 @@ class SettlementReportingAdapter extends BaseAdapter
         $path = "/settlement-reporting/v1/settlement-file/bounced-sub-merchant-rows" . QueryBuilder::build($request);
         return $this->httpGet($path);
     }
+
+    public function retrievePayoutDetails($id)
+    {
+        $path = "/settlement-reporting/v1/settlement-file/payout-details/" . $id;
+        return $this->httpGet($path);
+    }
 }
