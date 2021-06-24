ef38c5fee472a19a45aaf616766d2a89197adb8e
Alican Akkus <alican.akkus@craftbase.io>
2021-06-24 09:00:13 +0300

    make payment phase enum to uppercase

--- src/Model/PaymentPhase.php
+++ src/Model/PaymentPhase.php
@@ -4,7 +4,7 @@ namespace Craftgate\Model;
 
 class PaymentPhase
 {
-    const Auth = 'AUTH';
-    const PreAuth = 'PRE_AUTH';
-    const PostAuth = 'POST_AUTH';
+    const AUTH = 'AUTH';
+    const PRE_AUTH = 'PRE_AUTH';
+    const POST_AUTH = 'POST_AUTH';
 }
