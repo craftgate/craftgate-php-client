e7fd4c19d530f1350cddb14d4737be00420b0997
Yalin Gunayer <yalin.gunayer@gmail.com>
2021-11-26 10:53:19 +0300

    add loyalty endpoints (#43)

---
+++ samples/retrieve_loyalties.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('config/sample_config.php');
+
+$request = array(
+    'cardNumber' => '4043080000000003',
+    'expireYear' => '2044',
+    'expireMonth' => '07',
+    'cvc' => '000'
+);
+
+$response = SampleConfig::craftgate()->wallet()->retrieveLoyalties($request);
+
+print_r($response);
---
+++ src/Model/LoyaltyType.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Craftgate\Model;
+
+class LoyaltyType
+{
+    const REWARD_MONEY = 'REWARD_MONEY';
+}
