5770a3dd8ec1f0ebbb58c9bcdf870ea9b878204a
Onur Polattimur <onur.polattimur@gmail.com>
2022-01-18 11:25:19 +0300

    Add GarantiPay integration

--- src/Adapter/PaymentAdapter.php
+++ src/Adapter/PaymentAdapter.php
@@ -66,6 +66,12 @@ class PaymentAdapter extends BaseAdapter
         return $this->httpPost($path, $request);
     }
 
+    public function initGarantiPayPayment(array $request)
+    {
+        $path = "/payment/v1/garanti-pay-payments";
+        return $this->httpPost($path, $request);
+    }
+
     public function retrieveLoyalties(array $request)
     {
         $path = "/payment/v1/card-loyalties/retrieve";
--- src/Model/PaymentSource.php
+++ src/Model/PaymentSource.php
@@ -5,6 +5,6 @@ namespace Craftgate\Model;
 class PaymentSource
 {
     const API = 'API';
-    const MASTERPASS = 'MASTERPASS';
     const CHECKOUT_FORM = 'CHECKOUT_FORM';
+    const PAY_BY_LINK = 'PAY_BY_LINK';
 }
