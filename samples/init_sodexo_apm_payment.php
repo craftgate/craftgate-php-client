8ec11036dba80a94fe2ad9ca18505fb837c7f582
Alican Akkuş <alican.akkus@craftgate.io>
2022-06-14 12:13:56 +0300

    changes phone number

--- samples/init_sodexo_apm_payment.php
+++ samples/init_sodexo_apm_payment.php
@@ -16,7 +16,7 @@ $request = array(
     'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5',
     'externalId' => 'optional-externalId',
     'callbackUrl' => 'https://www.your-website.com/craftgate-apm-callback',
-    'apmUserIdentity' => '5545510009',
+    'apmUserIdentity' => '5555555555',
     'additionalParams' => array(
         'sodexoCode' => '843195'
     ),
