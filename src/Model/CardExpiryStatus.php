b657ec335da3423fc64c9bd9d8bf05153abc203c
Onur Polattimur <onur.polattimur@gmail.com>
2022-10-10 15:37:52 +0300

    Add CardExpiryStatus model (#63)

---
+++ src/Model/CardExpiryStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Craftgate\Model;
+
+class CardExpiryStatus
+{
+    const EXPIRED = 'EXPIRED';
+    const WILL_EXPIRE_NEXT_MONTH = 'WILL_EXPIRE_NEXT_MONTH';
+    const NOT_EXPIRED = 'NOT_EXPIRED';
+}
