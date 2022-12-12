aaee721c070923ffd8a70d0e39a2b4d59813ca08
Avni Balıkçı <avni.balikci@craftgate.io>
2022-12-12 17:35:43 +0300

    adds afterpay client sample

--- src/Model/ApmType.php
+++ src/Model/ApmType.php
@@ -9,6 +9,7 @@ class ApmType
     const SODEXO = 'SODEXO';
     const EDENRED = 'EDENRED';
     const PAYPAL = 'PAYPAL';
+    const AFTERPAY = 'AFTERPAY';
     const FUND_TRANSFER = 'FUND_TRANSFER';
     const CASH_ON_DELIVERY = 'CASH_ON_DELIVERY';
 }
