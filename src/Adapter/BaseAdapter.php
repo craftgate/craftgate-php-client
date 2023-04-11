404fa54fe833de72e95be83be73aa3697aff422a
Sabri Onur Tüzün <onur@craftgate.io>
2023-04-11 10:15:17 +0300

    prepares for next release v1.0.32

--- src/Adapter/BaseAdapter.php
+++ src/Adapter/BaseAdapter.php
@@ -56,7 +56,7 @@ class BaseAdapter
         $headers[] = 'x-api-key: ' . $this->options->getApiKey();
         $headers[] = 'x-rnd-key: ' . ($randomString = Guid::generate());
         $headers[] = 'x-auth-version: v1';
-        $headers[] = 'x-client-version: craftgate-php-client:1.0.31';
+        $headers[] = 'x-client-version: craftgate-php-client:1.0.32';
         $headers[] = 'x-signature: ' . Signature::generate(
                 $this->options, $path, $randomString, $request
             );
