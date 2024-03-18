a7fd4552d0fe61ee15af328afe55ca17adda5964
Onur Polattimur <onur.polattimur@gmail.com>
2024-03-18 16:13:49 +0300

    Add GarantiPay v2 integration

---
+++ samples/init_garanti_pay_pos_apm_payment.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once('config/sample_config.php');
+
+use Craftgate\Model\Currency;
+use Craftgate\Model\PaymentGroup;
+use Craftgate\Model\PosApmPaymentProvider;
+use Craftgate\Util\Guid;
+
+$request = array(
+    'price' => 100,
+    'paidPrice' => 100,
+    'currency' => Currency::TL,
+    'paymentGroup' => PaymentGroup::LISTING_OR_SUBSCRIPTION,
+    'conversationId' => '456d1297-908e-4bd6',
+    'paymentProvider' => PosApmPaymentProvider::GARANTI_PAY,
+    'callbackUrl' => 'https://www.your-website.com/craftgate-pos-apm-callback',
+    'items' => array(
+        array(
+            'externalId' => Guid::generate(),
+            'name' => 'Item 1',
+            'price' => 30
+        ),
+        array(
+            'externalId' => Guid::generate(),
+            'name' => 'Item 2',
+            'price' => 50
+        ),
+        array(
+            'externalId' => Guid::generate(),
+            'name' => 'Item 3',
+            'price' => 20
+        )
+    ),
+    'additionalParams' => array(
+        'integrationType' => "WEB2QR",
+    )
+);
+
+$response = SampleConfig::craftgate()->payment()->initPosApmPayment($request);
+
+print_r($response);
--- src/Model/PosApmPaymentProvider.php
+++ src/Model/PosApmPaymentProvider.php
@@ -5,6 +5,7 @@ namespace Craftgate\Model;
 class PosApmPaymentProvider
 {
     const YKB_WORLD_PAY = 'YKB_WORLD_PAY';
+    const YKB_WORLD_PAY_SHOPPING_LOAN = 'YKB_WORLD_PAY_SHOPPING_LOAN';
     const GOOGLEPAY = 'GOOGLEPAY';
-    const APPLEPAY = 'APPLEPAY';
+    const GARANTI_PAY = 'GARANTI_PAY';
 }
