c044f21de50b262ab723beca95cf650673ab5a53
Beran Santur <beran.santur@craftgate.io>
2024-03-20 13:45:26 +0300

    Add version of search payout completed tx with pagination (#105)

--- samples/search_settlement_payout_completed_transactions.php
+++ samples/search_settlement_payout_completed_transactions.php
@@ -7,7 +7,9 @@ use Craftgate\Model\SettlementType;
 $request = array(
     'settlementType' => SettlementType::SETTLEMENT,
     'startDate' => date_create()->modify('-1 days')->format('Y-m-d\TH:i:s'),
-    'endDate' => date_create()->format('Y-m-d\TH:i:s')
+    'endDate' => date_create()->format('Y-m-d\TH:i:s'),
+    'page' => 0,
+    'size' => 10
 );
 
 $response = SampleConfig::craftgate()->settlementReporting()->searchPayoutCompletedTransactions($request);
