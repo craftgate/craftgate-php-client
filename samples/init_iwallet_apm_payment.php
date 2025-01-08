81630c453f7971340b627e2997e4febedf99910a
Ertuğrul Berat ALLAHVERDİ <berat.allahverdi@hotmail.com>
2025-01-08 11:28:04 +0300

    Add iWallet as new APM

---
+++ samples/init_iwallet_apm_payment.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('config/sample_config.php');
+
+use Craftgate\Model\ApmType;
+use Craftgate\Model\Currency;
+use Craftgate\Model\PaymentGroup;
+use Craftgate\Util\Guid;
+
+$request = array(
+    'apmType' => ApmType::IWALLET,
+    'price' => 1,
+    'paidPrice' => 1,
+    'currency' => Currency::TL,
+    'paymentGroup' => PaymentGroup::LISTING_OR_SUBSCRIPTION,
+    'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5',
+    'externalId' => 'optional-externalId',
+    'callbackUrl' => 'https://www.your-website.com/craftgate-apm-callback',
+    'apmUserIdentity' => '6036819041742253',
+    'items' => array(
+        array(
+            'externalId' => Guid::generate(),
+            'name' => 'Item 1',
+            'price' => 0.40
+        ),
+        array(
+            'externalId' => Guid::generate(),
+            'name' => 'Item 2',
+            'price' => 0.60
+        )
+    )
+);
+
+$response = SampleConfig::craftgate()->payment()->initApmPayment($request);
+
+print_r($response);
