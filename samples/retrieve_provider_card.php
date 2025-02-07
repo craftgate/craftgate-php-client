f47840612f9df606771db55401a68764d4de4ad6
Semih Şahan <semih.sahan.info@gmail.com>
2025-02-07 11:17:19 +0300

    Update card provider field name

--- samples/retrieve_provider_card.php
+++ samples/retrieve_provider_card.php
@@ -6,7 +6,7 @@ $request = array(
     'providerCardToken' => '45f12c74-3000-465c-96dc-876850e7dd7a',
     'externalId' => 1001,
     'providerCardUserId' => '0309ac2d-c5a5-4b4f-a91f-5c444ba07b24',
-    'cardMigrationProvider' => 'MEX',
+    'cardProvider' => 'MEX',
 );
 
 $response = SampleConfig::craftgate()->payment()->retrieveProviderCards($request);
