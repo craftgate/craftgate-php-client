ec329e33d0686e4dc02ad2f80f065f4d34cc6662
Ertuğrul Berat ALLAHVERDİ <berat.allahverdi@hotmail.com>
2025-08-01 09:52:25 +0300

    Prepare for next release v1.0.48

--- src/Adapter/BaseAdapter.php
+++ src/Adapter/BaseAdapter.php
@@ -56,7 +56,7 @@ class BaseAdapter
         $headers[] = 'x-api-key: ' . $this->options->getApiKey();
         $headers[] = 'x-rnd-key: ' . ($randomString = Guid::generate());
         $headers[] = 'x-auth-version: v1';
-        $headers[] = 'x-client-version: craftgate-php-client:1.0.47';
+        $headers[] = 'x-client-version: craftgate-php-client:1.0.48';
         $headers[] = 'x-signature: ' . Signature::generate(
                 $this->options, $this->trimPath($path), $randomString, $request
             );
