8623edb987bf59578fde7dcda2cbecc3602a47ad
Serhat Tunca <serhat.tunca@craftgate.io>
2025-11-04 15:40:15 +0300

    Update fraud process for loyalty

--- samples/retrieve_loyalties.php
+++ samples/retrieve_loyalties.php
@@ -6,7 +6,15 @@ $request = array(
     'cardNumber' => '4043080000000003',
     'expireYear' => '2044',
     'expireMonth' => '07',
-    'cvc' => '000'
+    'cvc' => '000',
+    'clientIp' => '127.0.0.1',
+    'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5',
+    'fraudParams' => array(
+        'buyerEmail' => 'buyer@email.com',
+        'buyerPhoneNumber' => '905555555555',
+        'buyerExternalId' => 'buyerExternalId444',
+        'customFraudVariable' => 'sessionId213123',
+    )
 );
 
 $response = SampleConfig::craftgate()->payment()->retrieveLoyalties($request);
