a3aba1b0c7cd43f75ca872b199cce140510bae56
Ertuğrul Berat ALLAHVERDİ <berat.allahverdi@hotmail.com>
2025-12-31 16:27:59 +0300

    update

--- src/Model/PaymentAuthenticationType.php
+++ src/Model/PaymentAuthenticationType.php
@@ -7,4 +7,11 @@ class PaymentAuthenticationType
     const THREE_DS = 'THREE_DS';
     const NON_THREE_DS = 'NON_THREE_DS';
     const BKM_EXPRESS = 'BKM_EXPRESS';
+    const THREE_DS_FALLBACK_TO_NON_THREE_DS = 'THREE_DS_FALLBACK_TO_NON_THREE_DS';
+    const GOOGLEPAY = 'GOOGLEPAY';
+    const APPLEPAY = 'APPLEPAY';
+    const YKB_WORLD_PAY = 'YKB_WORLD_PAY';
+    const YKB_WORLD_PAY_SHOPPING_LOAN = 'YKB_WORLD_PAY_SHOPPING_LOAN';
+    const GARANTI_PAY = 'GARANTI_PAY';
+    const JUZDAN = 'JUZDAN';
 }
