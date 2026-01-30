776fe2052811e09d102acaf6180a96901e8e4525
furkanozmen <furkan.ozmen@craftgate.io>
2026-01-30 11:33:02 +0300

    Add store card secure fields functionality

--- samples/store_card_secure_fields.php
+++ samples/store_card_secure_fields.php
@@ -3,7 +3,7 @@
 require_once('config/sample_config.php');
 
 $request = array(
-    'secureFieldsToken' => 'tok-a158e3d1-179d-4ab4-a60f-032c760f31c7',
+    'secureFieldsToken' => 'xxXXxx',
 );
 
 $response = SampleConfig::craftgate()->payment()->storeCard($request);
