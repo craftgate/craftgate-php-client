ba5a398650b8f15b0ea9194b3d58751a09853cc9
Serhat Tunca <35580464+tuncaserhat@users.noreply.github.com>
2026-07-13 09:54:27 +0300

    Remove unused `paymentPhase` parameter from multi-payment initialization (#190)

--- samples/init_multi_payment.php
+++ samples/init_multi_payment.php
@@ -15,7 +15,6 @@ $request = array(
     'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5',
     'externalId' => Guid::generate(),
     'paymentGroup' => PaymentGroup::PRODUCT,
-    'paymentPhase' => PaymentPhase::AUTH,
     'items' => array(
         array(
             'name' => 'Item 1',
