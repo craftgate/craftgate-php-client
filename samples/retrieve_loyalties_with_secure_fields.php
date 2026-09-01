3fadbcfabbf18716d0f472f874649e4db4b935ed
Burak Yıldız <byoffical01@gmail.com>
2026-09-01 09:46:05 +0300

    Add secure fields token support to loyalty retrieve request (#196)

---
+++ samples/retrieve_loyalties_with_secure_fields.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once('config/sample_config.php');
+
+$request = array(
+    'secureFieldsToken' => 'xxXXxx',
+    'clientIp' => '127.0.0.1',
+    'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5'
+);
+
+$response = SampleConfig::craftgate()->payment()->retrieveLoyalties($request);
+
+print_r($response);
