e6af7b7315f84609de6c71bebbf3b4895020d6d1
Burak Yıldız <burak.yildiz@craftgate.io>
2026-09-25 16:03:36 +0300

    Add clientPort field to payment requests (#199)

--- samples/retrieve_loyalties.php
+++ samples/retrieve_loyalties.php
@@ -8,6 +8,7 @@ $request = array(
     'expireMonth' => '07',
     'cvc' => '000',
     'clientIp' => '127.0.0.1',
+    'clientPort' => 51520,
     'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5',
     'fraudParams' => array(
         'buyerEmail' => 'buyer@email.com',
--- samples/retrieve_loyalties_with_secure_fields.php
+++ samples/retrieve_loyalties_with_secure_fields.php
@@ -5,6 +5,7 @@ require_once('config/sample_config.php');
 $request = array(
     'secureFieldsToken' => 'xxXXxx',
     'clientIp' => '127.0.0.1',
+    'clientPort' => 51520,
     'conversationId' => '456d1297-908e-4bd6-a13b-4be31a6e47d5'
 );
 
